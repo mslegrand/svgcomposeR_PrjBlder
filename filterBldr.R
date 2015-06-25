b917205fccd8e2a6d96f5f24fbe78b44198d3c17
mark stephen legrand <coanalytics.net>
2015-06-25 15:27:53 -0400

    list of filter elements

---
+++ filterBldr.R
@@ -0,0 +1,23 @@
+
+feElementsResult<-c(
+  'feBlend', 'feColorMatrix', 'feComponentTransfer', 
+  'feComposite', 'feConvolveMatrix', 'feDiffuseLighting', 
+  'feDisplacementMap', 'feFlood', 'feGaussianBlur', 
+  'feImage', 'feMerge', 'feMorphology', 
+  'feOffset', 'feSpecularLighting', 'feTile', 
+  'feTurbulence')
+
+feElementsAll<-c(
+  'feDistantLight','fePointLight','feSpotLight',
+  'feBlend','feColorMatrix','feComponentTransfer',
+  'feFuncR','feFuncG','feFuncB','feFuncA',
+  'feComposite','feConvolveMatrix','feDiffuseLighting',
+  'feDisplacementMap','feFlood','feGaussianBlur',
+  'feImage','feMerge','feMergeNode','feMorphology','feOffset',
+  'feSpecularLighting','feTile','feTurbulence')
+
+feElementsIn<-c(
+  'feConvolveMatrix','feDiffuseLighting','feOffset',
+  'feBlend','feColorMatrix','feComponentTransfer',
+  'feComposite','feDisplacementMap','feGaussianBlur',
+  'feMorphology','feSpecularLighting','feTile')
